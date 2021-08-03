0504426d0c3c7d86e42fed72201cab0fd29b0fd4
venkatesh-sridharan <vsridharan@privco.com>
2021-08-03 11:18:08 -0400

    Changes in connect_db file

--- connect_db.py
+++ connect_db.py
@@ -11,19 +11,14 @@ host = config('host')
 port = config('port')
 db = config('db')
 
-# engine = sal.create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format('root',password,host,port,db))
-# conn = engine.connect()
-
-# q = engine.execute('''select cp.id, cp.name, cw.url from company_profile as cp 
-#                     join company_website as cw on cp.id = cw.company_profile_id
-#                     order by cp.id ;''')
-# query_result = q.fetchall()
-
-engine = sal.create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format('root',password,host,port,db))
+engine = sal.create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(user,password,host,port,db))
 conn = engine.connect()
-q = engine.execute('''select * from company_id_and_websites''')
+
+q = engine.execute('''select cp.id, cp.name, cw.url from company_profile as cp 
+                    join company_website as cw on cp.id = cw.company_profile_id
+                    order by cp.id ;''')
 query_result = q.fetchall()
 
 with open('company_id_and_websites_test.txt','w',encoding='utf8') as wf:
     for row in query_result:
-        wf.write('\t'.join(list(row))+'\n')
+        wf.write('\t'.join([str(x) for x in list(row)])+'\n')
